fbd59325e7264cbd099ceceed4fc09b9f79d391e
Yerlanuly Adiyet <erlanulyadiet0@gmail.com>
2025-04-10 17:24:21 +0500

    Add Lab7: Django project setup with virtual environment and admin configuration

---
+++ Lab3/Task1/ifelse.js
@@ -0,0 +1,30 @@
+if ("0") {
+    alert( 'Hello' );
+  }
+
+
+  let value = prompt('Type a number', 0);
+
+if (value > 0) {
+  alert( 1 );
+} else if (value < 0) {
+  alert( -1 );
+} else {
+  alert( 0 );
+}
+
+
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+let result = (a + b < 4) ? 'Below' : 'Over';
+
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
---
+++ Lab3/Task1/variables.js
@@ -0,0 +1,22 @@
+let admin, userName;
+
+userName = "Adi";
+
+admin = userName;
+
+alert( admin ); 
+
+let ourPlanetName = "Earth";
+
+let currentUserName = "Adi";
+
+let name = "Ilya";
+
+// the expression is a number 1
+alert( `hello ${1}` ); // hello 1
+
+// the expression is a string "name"
+alert( `hello ${"name"}` ); // hello name
+
+// the expression is a variable, embed it
+alert( `hello ${name}` ); // hello Ilya
